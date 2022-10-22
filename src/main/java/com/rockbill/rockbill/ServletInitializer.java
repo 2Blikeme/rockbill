9e6f2ef7ad8e7aaf9538f5b7d0fdea7d71d9e97b
d.marusev <d.marusev@it-factoria.ru>
2022-10-22 15:42:40 +0300

    Initial commit

---
+++ src/main/java/com/rockbill/rockbill/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.rockbill.rockbill;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RockbillApplication.class);
+    }
+
+}
---
+++ src/test/java/com/rockbill/rockbill/RockbillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rockbill.rockbill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockbillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
